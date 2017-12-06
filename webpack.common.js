eedfb8f1b2d741a3d4235ea103949ff652b1a630
OldGreg <flyers08@vt.edu>
2017-12-06 12:09:10 -0500

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+//const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
